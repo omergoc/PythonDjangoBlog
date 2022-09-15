11d58ff93825881046114348e21f65651368ba97
Ömer Göç <74094920+omergoc@users.noreply.github.com>
2022-09-15 17:14:54 +0300

    Update production.py

--- blogapp/settings/production.py
+++ blogapp/settings/production.py
@@ -2,7 +2,7 @@ from blogapp.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["siberatay.com"]
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
    'default': {
