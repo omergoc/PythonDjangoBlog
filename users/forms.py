55a1dcaa520eabd743b602be773d02b5d68b732f
Ömer Göç <74094920+omergoc@users.noreply.github.com>
2022-09-24 19:44:23 +0300

    development

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,7 @@ from captcha.widgets import ReCaptchaV2Checkbox,ReCaptchaV2Invisible
 
 
 class LoginForm(forms.Form):
-    captcha = ReCaptchaField()
+    #captcha = ReCaptchaField()
     username = forms.CharField(widget=forms.TextInput(attrs={
         'class':'form-control',
         'placeholder':'Kullanıcı Adı'
@@ -16,7 +16,7 @@ class LoginForm(forms.Form):
     }))
 
 class RegisterForm(forms.Form):
-    captcha = ReCaptchaField()
+    #captcha = ReCaptchaField()
     username = forms.CharField(widget=forms.TextInput(attrs={
         'class':'form-control',
         'placeholder':'Kullanıcı Adı'
