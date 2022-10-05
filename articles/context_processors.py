a9383c4c885394bb71671ca520fea5f5ae5bc062
Ömer Göç <74094920+omergoc@users.noreply.github.com>
2022-10-05 23:38:15 +0300

    update portal

--- articles/context_processors.py
+++ articles/context_processors.py
@@ -1,4 +1,4 @@
-from .models import Categories,Comments, Videos, News
+from .models import Categories,Comments, Videos,News,Articles
 from settings import models
 
 
@@ -7,7 +7,11 @@ def categories_renderer(request):
     settings = models.Setting.objects.last()
     comms = Comments.objects.order_by('-id')[:3]
     videos = Videos.objects.filter(available=True).order_by('-id')[:3]
-    news = Categories.objects.filter(name="Siberatay Haberleri").order_by('-id')[:3]
+    category = Categories.objects.get(name="Siberatay Haberleri")
+    article_list = list(Articles.objects.filter(category=category.id,available=True).order_by('-id')[:1])
+    video_list = list(Videos.objects.filter(category=category.id,available=True).order_by('-id')[:1])
+    new_list = list(News.objects.filter(category=category.id,available=True).order_by('-id')[:1])
+    article_list = article_list + new_list + video_list
     categories = Categories.objects.all()
 
-    return { 'all_categories': categories, 'sidebar_videos': videos, 'sidebar_news': news, 'sidebar_comms': comms, 'settings': settings}
+    return { 'all_categories': categories, 'sidebar_videos': videos, 'sidebar_news': article_list, 'sidebar_comms': comms, 'settings': settings}
