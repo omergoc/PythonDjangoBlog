6e8025ae246471942efdc1feab7634093280b140
omergoc <omer.1998.tr@outlook.com>
2022-10-07 16:56:11 +0300

    development

--- articles/context_processors.py
+++ articles/context_processors.py
@@ -7,13 +7,13 @@ def categories_renderer(request):
     settings = models.Setting.objects.last()
     comms = Comments.objects.order_by('-id')[:3]
     videos = Videos.objects.filter(available=True).order_by('-id')[:3]
-    """
+
     category = Categories.objects.get(id=4)
     article_list = list(Articles.objects.filter(category=category.id,available=True).order_by('-id')[:1])
     video_list = list(Videos.objects.filter(category=category.id,available=True).order_by('-id')[:1])
     new_list = list(News.objects.filter(category=category.id,available=True).order_by('-id')[:1])
     article_list = article_list + new_list + video_list
-    """
+
     categories = Categories.objects.all()
 
-    return { 'all_categories': categories, 'sidebar_videos': videos, 'sidebar_news': 'article_list', 'sidebar_comms': comms, 'settings': settings}
+    return { 'all_categories': categories, 'sidebar_videos': videos, 'sidebar_news': article_list, 'sidebar_comms': comms, 'settings': settings}
