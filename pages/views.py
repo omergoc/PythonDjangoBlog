ad19d197957efe3f0ce9f9f03e0dc38a749a4a79
omergoc <omer.1998.tr@outlook.com>
2022-10-07 16:44:13 +0300

    development

--- pages/views.py
+++ pages/views.py
@@ -35,12 +35,7 @@ def handler404(request, exception=None):
 @care_control
 def index(request):
     slider_article = Slider.objects.order_by('-id')[:2]
-    last_articles = list(Articles.objects.filter(available=True).order_by('-id')[:5])
-    last_videos = list(Videos.objects.filter(available=True).order_by('-id')[:5])
-    last_news = list(News.objects.filter(available=True).order_by('-id')[:5])
-    article_list = last_articles + last_news + last_videos
     context = {
-        'articles': article_list,
         'slider_article': slider_article
     }
     return render(request, 'index.html', context)
