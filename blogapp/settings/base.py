3405f3f618123cd057a7544c7ec1e285f7140542
Ömer Göç <74094920+omergoc@users.noreply.github.com>
2022-11-12 22:05:02 +0300

    update

--- blogapp/settings/base.py
+++ blogapp/settings/base.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'material',
     'material.admin',
-    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
