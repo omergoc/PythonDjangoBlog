a2fca992276f225c799933996047ca6a16bbdc9c
Ömer Göç <74094920+omergoc@users.noreply.github.com>
2022-11-25 10:36:33 +0300

    web servis
    update

--- users/api/views.py
+++ users/api/views.py
@@ -18,10 +18,10 @@ class AccountView(RetrieveUpdateAPIView):
         queryset = self.get_queryset()
         obj = get_object_or_404(queryset, id = self.request.user.id)
         return obj
-
+    """
     def perform_update(self, serializer):
         serializer.save(user = self.request.user) 
-
+    """
 class UpdatePassowrd(APIView):
     permission_classes = (IsAuthenticated,)
 
